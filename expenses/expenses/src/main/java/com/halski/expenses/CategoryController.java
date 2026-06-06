78f9c15fd1e2e5f18d463f0176e7afa929cb0263
James H <jphall33904@gmail.com>
2026-06-05 21:02:30 -0500

    can now update categories
    Category updates are now possible via a PUT request

--- expenses/expenses/src/main/java/com/halski/expenses/CategoryController.java
+++ expenses/expenses/src/main/java/com/halski/expenses/CategoryController.java
@@ -3,8 +3,13 @@ package com.halski.expenses;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -17,7 +22,29 @@ public class CategoryController {
     }
 
     @PostMapping
-    public Category create(@RequestBody String name, @RequestParam(required = false) String description) {
-        return categoryService.create(name, description);
+    public Category create(@RequestBody CreateCategory request) {
+        return categoryService.create(request.name(), request.description());
     }
+
+    @PutMapping("/{id}")
+    public Category update(
+        @PathVariable Long id,
+        @RequestBody UpdateCategory request) {
+        return categoryService.update(id, request.name(), request.description());
+    }
+
+    @GetMapping({"", "/"})
+    public List<Category> getAll() {
+        return categoryService.getAll();
+    }
+
+    public static record UpdateCategory(
+        @JsonProperty String name,
+        @JsonProperty String description
+    ) {}
+
+    public static record CreateCategory(
+        @JsonProperty String name,
+        @JsonProperty String description
+    ) {}
 }
--- expenses/expenses/src/main/java/com/halski/expenses/CategoryService.java
+++ expenses/expenses/src/main/java/com/halski/expenses/CategoryService.java
@@ -19,6 +19,16 @@ public class CategoryService {
         return categoryRepository.save(category);
     }
 
+    public Category update(Long id, String name, String description) {
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category Not Found"));
+
+        category.setName(name);
+        category.setDescription(description);
+
+        return categoryRepository.save(category);
+    }
+
     public List<Category> getAll() {
         return categoryRepository.findAll();
     }
